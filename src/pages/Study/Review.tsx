3c20d05b9e0a65d523004c1fb87633f13f9ee583
jhdgo1225 <parkjh99365@gmail.com>
2025-07-12 21:04:22 +0900

    deploy: develop 재선언 부분 해소

--- src/pages/Study/Review.tsx
+++ src/pages/Study/Review.tsx
@@ -9,11 +9,6 @@ const ReviewStyle = styled.div`
   padding: 0 24px;
 `;
 
-const ReviewStyle = styled.div`
-  margin-top: 18px;
-  padding: 0 24px;
-`;
-
 const Review = () => {
   const [reviews, setReviews] = useState<Record<string, any>[]>([]);
 
